d5b85edd46a9af4010942a8bda2a6b227053079c
walchko <walchko@users.noreply.github.com>
2020-07-12 18:17:30 -0400

    Update utils.py

--- numpy_camera/utils.py
+++ numpy_camera/utils.py
@@ -11,5 +11,6 @@ bgrw = np.array([0.1140, 0.5870, 0.2989])
 rgb2gray = lambda im: np.average(rgb, weights=rgbw, axis=2).astype(np.uint8)
 bgr2gray = lambda im: np.average(rgb, weights=bgrw, axis=2).astype(np.uint8)
 
-def gray2rgb(g): a=g.copy().T; return np.array([a,a,a],dtype=np.uint8).T
+# def gray2rgb(g): a=g.copy().T; return np.array([a,a,a],dtype=np.uint8).T.copy()
+gray2rgb = lambda g: np.array([g.T,g.T,g.T],dtype=np.uint8).T.copy()
 gray2bgr = lambda g: gray2rgb(g)
